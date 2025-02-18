882a6e264bf9491b1760369599e98ffe2a5ebb2f
Matthew Shumaker <matthewshu22@gmail.com>
2025-02-18 01:31:29 -0500

    moves social icons to bottom if frame is too narrow

--- includes/header.js
+++ includes/header.js
@@ -1,6 +1,8 @@
 const headerContent = `
   <div class="container">
-    <a href="index.html" style="font-family: Georgia; text-decoration: none; color: inherit;"><h1>Matthew Shumaker</h1></a>
+    <div class="top-left">
+      <a href="index.html" style="font-family: Georgia; text-decoration: none; color: inherit;"><h1>Matthew Shumaker</h1></a>
+    </div>
     <div class="top-right">
       <a href="https://www.instagram.com/matthewshumakr/"><i class="fa-brands fa-square-instagram"></i></a>
       <a href="https://www.youtube.com/@shumakerm"><i class="fa-brands fa-square-youtube"></i></a>
