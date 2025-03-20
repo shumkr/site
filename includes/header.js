6f6183d24a3090cfe140a149f1822a1b8660a407
shumkr <matthewshu22@gmail.com>
2025-03-19 22:02:02 -0400

    Renamed a couple things (.top-right to .socials) and added copyright because we love maintaining the rights to our work

--- includes/header.js
+++ includes/header.js
@@ -3,7 +3,7 @@ const headerContent = `
     <div class="top-left">
       <a href="index.html" style="font-family: Georgia; text-decoration: none; color: inherit;"><h1>Matthew Shumaker</h1></a>
     </div>
-    <div class="top-right">
+    <div class="socials">
       <a href="https://www.instagram.com/matthewshumakr/"><i class="fa-brands fa-square-instagram"></i></a>
       <a href="https://www.youtube.com/@shumakerm"><i class="fa-brands fa-square-youtube"></i></a>
       <a href="https://www.github.com/shumkr"><i class="fa-brands fa-square-github"></i></a>
